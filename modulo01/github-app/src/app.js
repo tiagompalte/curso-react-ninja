28f75ffa5123120bf7b9be29c081e2c66591fba4
Tiago M Palte <tiagompalte@hotmail.com>
2019-06-01 10:06:44 -0300

    Bind do this em eventos

--- modulo01/github-app/src/app.js
+++ modulo01/github-app/src/app.js
@@ -14,6 +14,8 @@ class App extends Component {
       starred: [],
       isFetching: false
     }
+
+    this.handleSearch = this.handleSearch.bind(this);
   }
 
   getGitHubApiUrl(username, type) {
@@ -70,7 +72,7 @@ class App extends Component {
       repos={this.state.repos}
       starred={this.state.starred}
       isFetching={this.state.isFetching}
-      handleSearch={(e) => this.handleSearch(e)}
+      handleSearch={this.handleSearch}
       handleRepos={this.handleReposStarred('repos')}
       handleStarred={this.handleReposStarred('starred')}
     />
